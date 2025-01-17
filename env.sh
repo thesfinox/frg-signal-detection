4d58236ef382e863d9217d0e946912d6e18ecb50
Riccardo Finotello <riccardo.finotello@cea.fr>
2025-01-17 15:55:10 +0100

    Update environment

--- env.sh
+++ env.sh
@@ -13,6 +13,7 @@ export FRG_INSTALLDIR=$HOME/Code/frg-signal-detection
 export FRG_CONFIGPATH=${FRG_INSTALLDIR}/configs
 export FRG_DOCSPATH=${FRG_INSTALLDIR}/docs
 export FRG_SRCPATH=${FRG_INSTALLDIR}/src
+export FRG_SCRIPTSPATH=${FRG_INSTALLDIR}/scripts
 export FRG_TESTPATH=${FRG_INSTALLDIR}/tests
 
 export SCRATCHDIR=${SCRATCHDIR:-${HOME}/Datasets}
