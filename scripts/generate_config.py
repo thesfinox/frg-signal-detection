7a271f79cd7380dbdfa0b2eb3bb164e333b867c0
Riccardo Finotello <riccardo.finotello@cea.fr>
2025-01-28 16:10:30 +0100

    Add more logging

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -48,7 +48,7 @@ def main(a: argparse.Namespace) -> int | str:
     if not params.exists():
         logger.error("Parameters file %s does not exist!", a.params)
         raise FileNotFoundError("Parameters file %s does not exist!" % a.params)
-    logger.debug("Opening parameters file %s")
+    logger.debug("Opening parameters file %s", a.params)
     with open(str(params)) as f:
         params = json.load(f)
 
