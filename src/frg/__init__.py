4fff5f46194e8ebb770305f9be293a1912127505
Riccardo Finotello <riccardo.finotello@cea.fr>
2026-03-26 10:22:48 +0100

    chore(release): bump version to v2.1.2

--- src/frg/__init__.py
+++ src/frg/__init__.py
@@ -33,7 +33,7 @@ from frg.utils.analysis import (
 from frg.utils.utils import get_cfg_defaults, get_logger, load_data
 
 # Set the version number
-__version__ = "v2.1.1"
+__version__ = "v2.1.2"
 
 # Set the author
 __author__ = "Riccardo Finotello"
