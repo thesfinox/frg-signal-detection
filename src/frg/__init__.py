b6a2dd2810ffb6c88e811edb323df6de43b4e2b5
Riccardo Finotello <riccardo.finotello@cea.fr>
2026-04-23 09:03:38 +0200

    chore: update metadata
    - Update metadata with correct licensing and authors
    
    - Automatically handle metadata from pyproject

--- src/frg/__init__.py
+++ src/frg/__init__.py
@@ -3,25 +3,9 @@
 The work is based on `theoretical advancements <https://arxiv.org/abs/2201.04250>`__ and previous `numerical works <https://arxiv.org/abs/2310.07499>`__ and `improvements <https://arxiv.org/abs/2507.01064>`__.
 """
 
+from email.utils import getaddresses
 from importlib.metadata import PackageNotFoundError, metadata
 
-try:
-    _meta = metadata("frg-signal-detection")
-    __version__ = _meta["Version"]
-    __author__ = _meta["Author-email"].split("<")[0].strip().rstrip(",").strip()
-    __email__ = _meta["Author-email"].split("<")[-1].rstrip(">").strip()
-    __url__ = _meta.get(
-        "Project-URL", "https://github.com/thesfinox/frg-signal-detection"
-    )
-except PackageNotFoundError:
-    __version__ = "0.0.0-unknown"
-    __author__ = "Riccardo Finotello"
-    __email__ = "riccardo.finotello@cea.fr"
-    __url__ = "https://github.com/thesfinox/frg-signal-detection"
-
-__license__ = "CEA Proprietary License"
-
-# Import the modules
 from frg.distributions.distributions import (
     EmpiricalDistribution,
     MarchenkoPastur,
@@ -50,6 +34,49 @@ from frg.utils.analysis import (
     plot_trajectories,
 )
 from frg.utils.utils import get_cfg_defaults, get_logger, load_data
+
+
+def _parse_authors_and_emails(author_email_field: str) -> tuple[str, str]:
+    """Parse a metadata Author-email field into author and email lists.
+
+    Returns:
+        A pair of comma-separated strings: authors, emails.
+    """
+    parsed = getaddresses([author_email_field])
+    if not parsed:
+        return "", ""
+
+    authors = ", ".join(name for name, _ in parsed if name)
+    emails = ", ".join(email for _, email in parsed if email)
+
+    return authors, emails
+
+
+try:
+    _meta = metadata("frg-signal-detection")
+
+    # Parse version
+    __version__: str = _meta["Version"]
+
+    # Parse author(s) and email(s)
+    __author__: str
+    __email__: str
+    __author__, __email__ = _parse_authors_and_emails(
+        _meta.get("Author-email", "")
+    )
+    __author__ = __author__ or _meta.get("Author", "")
+
+    # Parse URL
+    __url__: str = _meta.get(
+        "Project-URL", "https://github.com/thesfinox/frg-signal-detection"
+    )
+except PackageNotFoundError:
+    __version__ = "0.0.0-unknown"
+    __author__ = "Riccardo Finotello, Parham Radpay"
+    __email__ = "riccardo.finotello@cea.fr, parhamradpay@gmail.com"
+    __url__ = "https://github.com/thesfinox/frg-signal-detection"
+
+__license__ = "MIT License"
 
 # Package imports
 __all__ = [
